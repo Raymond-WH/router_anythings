793c323ff1e9d433f8d7ceb365339693a45f18bc
水木年华 <2461981896@qq.com>
2022-06-18 10:36:36 +0800

    history路由+popstate+pushstate

--- src/App.js
+++ src/App.js
@@ -13,18 +13,45 @@ export default function App() {
   //     setPath(window.location.hash.slice(1))
   //   })
   // })
+  // history模式
+  const [path, setPath] = useState(window.location.pathname)
+  useEffect(() => {
+    window.addEventListener('popstate', () => {
+      console.log('history值变化');
+      setPath(window.location.pathname)
+    })
+  }, [])
+  // 设置路由跳转
+  const push = (path) => {
+    window.history.pushState({}, '', path)
+    setPath(path)
+  }
   return (
     <div className="app">
       <h1>更组件</h1>
       <ul>
-        <li><a href="#/login">登录</a></li>
+        {/* <li><a href="#/login">登录</a></li>
         <li><a href="#/home">首页</a></li>
-        <li><a href="#/user">用户</a></li>
+        <li><a href="#/user">用户</a></li> */}
+        <li><a href="/login"
+          onClick={(e) => {
+            e.preventDefault()
+            push('/login')
+          }}
+        >登录</a></li>
+        <li><a href="/home" onClick={(e) => {
+          e.preventDefault()
+          push('/home')
+        }}>首页</a></li>
+        <li><a href="/user" onClick={(e) => {
+          e.preventDefault()
+          push('/user')
+        }}>用户</a></li>
       </ul>
       <hr></hr>
-      {/* {path === '/login' && <Login />}
+      {path === '/login' && <Login />}
       {path === '/home' && <Home />}
-      {path === '/user' && <User />} */}
+      {path === '/user' && <User />}
     </div>
   )
 }
