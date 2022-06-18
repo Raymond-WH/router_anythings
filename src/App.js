de57dc96fa6717419e8b96b4f0abad032a5b935c
水木年华 <2461981896@qq.com>
2022-06-18 09:52:04 +0800

    hash路由监听2

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Login from "./pages/Login"
 import User from "./pages/User"
 
 export default function App() {
-  const [path, setPath] = useState(window.location.hash.slice(1))
-  useEffect(() => { 
-    window.addEventListener('hashchange', () => { 
-      console.log('hash值变化');
-      setPath(window.location.hash.slice(1))
-    })
-  })
+  // hash模式
+  // const [path, setPath] = useState(window.location.hash.slice(1))
+  // useEffect(() => { 
+  //   window.addEventListener('hashchange', () => { 
+  //     console.log('hash值变化');
+  //     setPath(window.location.hash.slice(1))
+  //   })
+  // })
   return (
     <div className="app">
       <h1>更组件</h1>
@@ -21,9 +22,9 @@ export default function App() {
         <li><a href="#/user">用户</a></li>
       </ul>
       <hr></hr>
-      {path === '/login' && <Login />}
+      {/* {path === '/login' && <Login />}
       {path === '/home' && <Home />}
-      {path === '/user' && <User />}
+      {path === '/user' && <User />} */}
     </div>
   )
 }
